78014a8d3f4fb273c833a380b21128b4d4578119
Turash Jurij <yrik.turash@gmail.com>
2015-07-17 09:15:37 +0300

    Add DTO layer

---
+++ LetsDo/LetsDo.DTO/ViewModels/IndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LetsDo.DTO.ViewModels
+{
+    public class IndexViewModel
+    {
+        public IList<Issue> Issues { get; set; }
+        public IList<UnderIssue> UnderIssues { get; set; }
+        public IList<Category> CategoriesList { get; set; }
+        public int? ActiveCategoryId { get; set; }
+    }
+}
